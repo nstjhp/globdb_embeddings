103b2b15e3188fe9fa823e94bec38cc3f4faf1dc
Nick Pullen <5261752+nstjhp@users.noreply.github.com>
2025-06-03 19:57:47 +0200

    Update after Daan replaced keys file with 2nd col as explicit index

--- py_bash_scripts/extract_embeddings_using_keys_txt_file.py
+++ py_bash_scripts/extract_embeddings_using_keys_txt_file.py
@@ -4,15 +4,19 @@ import numpy as np
 
 def build_inmemory_index(keys_txt_path: str):
     """
-    One-time build: reads keys_txt_path line by line into a dict.
-    Returns { key_str: line_number_zero_based }.
-    Takes around 1 minute to build.
+    One‐time build: reads the 2‐column TSV (key \t index)
+    Returns: { key_str: (int(index) - 1) }
+
+    Avoid per‐line list allocations with partition (returns a 3‐tuple of (before, sep, after))
+    Takes around 80 seconds to build.
     """
     idx_map = {}
     with open(keys_txt_path, 'r', encoding='utf-8') as f:
-        for i, line in enumerate(f):
-            key = line.rstrip('\n')
-            idx_map[key] = i
+        for line in f:
+            raw = line[:-1] # cheaper than rstrip('\n') but assumes every line ends in newline
+            # partition at the first '\t'
+            key, _, idx_str = raw.partition('\t')
+            idx_map[key] = int(idx_str) - 1
     return idx_map
 
 def _grep_index(keys_txt_path: str, query_id: str):
@@ -21,17 +25,17 @@ def _grep_index(keys_txt_path: str, query_id: str):
     or raises KeyError if not found.
     grep -n returns format like 123456:ID_QUERY
     """
-    # -x : exact match, -n : prefix each line with its 1-based line number
+    # -n : prefix each line with its 1-based line number
     proc = subprocess.run(
-        ['grep', '-nx', query_id, keys_txt_path],
+        ['grep', '-n', query_id, keys_txt_path],
         capture_output=True, text=True
     )
     if proc.returncode != 0 or not proc.stdout:
         # !r tells Python to call repr() on query_id rather than str()
         # e.g. with quotes in case there is newlines, or non-printable characters
         raise KeyError(f"{query_id!r} not found in keys file")
-    # stdout like "12345:KEY\n"
-    lineno = int(proc.stdout.split(':', 1)[0])
+    # stdout like "12345:KEY\t6789\n"
+    lineno = int(proc.stdout.split('\t', 1)[1].rstrip())
     return lineno - 1  # convert to 0-based
 
 def get_embeddings_multi(
@@ -90,8 +94,8 @@ def get_embeddings_multi(
 # example usages
 if __name__ == "__main__":
     # Load embeddings and the corresponding text file of keys
-    h5_path        = "GlobDB40.h5"
-    keys_txt_path  = "GlobDB40keys.txt"
+    h5_path        = "/lisc/fileshare/globdb/globdb_r226/pLM_embeddings/globdb_r226_linclust_2plus_ProtT5_embeddings.h5"
+    keys_txt_path  = "globdb_r226_linclust_2plus_ID_index.tsv"
 
     # For a single ID or list of IDs:
     query_ids      = ["BCRBG_15609___2917", "GCA_013288945___541", "MOTU40_023145___1271"]  
